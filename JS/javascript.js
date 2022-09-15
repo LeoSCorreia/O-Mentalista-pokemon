50167dce60905f673db37c0a61d1e0936a331de5
LeoSCorreia <leandrocorreia.300@gmail.com>
2022-09-15 00:11:21 -0300

    ultima atualização

--- JS/javascript.js
+++ JS/javascript.js
@@ -4,10 +4,14 @@ function adivinhe() {
     var resultado = document.getElementById('resultado');
 
     if (chute == numeroSecreto) {
-    } else if (chute > 10 || chute < 0) {
-        resultado.textContent =
-            'Digite um número entre 0 e 10 para continuiar.';
+        resultado.textContent = 'Parabéns você acertou!';
+    } else if (chute > 10 || chute < 0 || !chute) {
+        resultado.innerHTML =
+            'Digite um número entre <strong>0</strong> e <strong>10</strong> para continuiar.';
     } else {
-        console.log('Você errou');
+        resultado.innerHTML =
+            '<p>Você errou! O número correto era <strong>' +
+            numeroSecreto +
+            '</strong></p>';
     }
 }
